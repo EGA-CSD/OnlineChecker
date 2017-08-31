ddf5758294fa0df213c7690284c261297908454b
pepsi7959 <pepsi_kmutnb@windowslive.com>
2017-08-31 11:31:21 +0700

    support no unread message.

--- checkNewEmail.php
+++ checkNewEmail.php
@@ -5,11 +5,16 @@
   function check_new_message(){
     $token = getToken("narongsak@ega.or.th", "Acho08mkr", "https://accounts.mail.go.th/service/soap");
     $folder = getFolderRequest($token, "https://accounts.mail.go.th/service/soap");
-    echo $folder;
+    //echo $folder;
     $begin = strpos($folder,' u="') + strlen(' u="');
-    $end = strpos($folder, '" name="');
-    $num_msg = substr($folder, $begin, $end-$begin);
-    return "You've got ".$num_msg." new emails";
+    if( $begin > 4 ){
+      $end = strpos($folder, '" name="');
+      $num_msg = substr($folder, $begin, $end-$begin);
+      return "You've got ".$num_msg." new messages";
+    }else{
+      return "You haven't got a new message.";
+    }
+
   }
   
 ?>  
